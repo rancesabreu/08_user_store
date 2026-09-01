72371b3af3e3e78ec48bf2a8d46f02b2737af944
Josue Abreu <rancesabreu@gmail.com>
2026-09-01 10:13:33 -0400

    Semilla JWToken

--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -1,27 +1,30 @@
-import jwt from 'jsonwebtoken';
+import jwt, { Secret } from 'jsonwebtoken';
 import { envs } from './envs';
 
-const JWT_SEED = envs.JWT_SEED;
+const JWT_SEED: Secret = envs.JWT_SEED;
 
 export class JwtAdapter {
 
-    static async generateToken(payload: object, duration: jwt.SignOptions['expiresIn'] = '2h') {
+    // DI
+    static async generateToken(payload: any, duration: string = '2h') {
 
-        return new Promise<string | null>((resolve) => {
-            jwt.sign(payload, JWT_SEED, { expiresIn: duration }, (err, token) => {
-                if (err) return resolve(null);
-                resolve(token ?? null);
+        return new Promise((resolve) => {
+            jwt.sign(payload, JWT_SEED, {expiresIn: duration as any}, (err, token) => {
+
+                if(err) return resolve(null);
+
+                resolve(token)
+    
             });
-        });
+        })
+
+
     }
 
-    static async validateToken<T>(token: string): Promise<T | null> {
-        return new Promise((resolve) => {
-            jwt.verify(token, JWT_SEED, (err, decoded) => {
-                if (err) return resolve(null);
-                resolve((decoded ?? null) as T | null);
-            });
-        });
+    static validateToken(token: string) {
+        
+        throw new Error ('Method not implemented.');
+        return;
     }
 
 }
