7478b1c2f2b6191eb0e1abaec85c4101bcc033df
Josue Abreu <rancesabreu@gmail.com>
2026-09-03 12:21:45 -0400

    Preparación de los modelos restantes, categorias con sus rutas y controladores - AuthMiddleware

---
+++ src/presentation/category/routes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { CategoryController } from "./controller";
+
+
+
+export class CategoryRoutes {
+
+
+  static get routes(): Router {
+
+    const router = Router();
+    const controller = new CategoryController();
+    
+    // Definir las rutas
+    router.get('/', controller.getCategories); //obtener todas las categorías
+    router.post('/', controller.createCategory); // crear una nueva categoría
+
+
+
+    return router;
+  }
+
+
+}
+
